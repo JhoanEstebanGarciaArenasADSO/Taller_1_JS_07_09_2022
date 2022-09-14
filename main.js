64622f95cb0ef8cc97af6241ccb878ebd6d548c1
JhoanEstebanGarciaArenasADSO <>
2022-09-14 07:36:25 -0500

    Ejercicio 9

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 addEventListener("DOMContentLoaded", ()=>{
-// 8. Escribir un programa que evalúe la siguiente expresión:
-//(a+7*c)/(b+2-a)+2*b 
-let a = Number(prompt("Ingrese A", 7));
-let b = Number(prompt("Ingrese B", 7));
-let c = Number(prompt("Ingrese C", 7));
-let formu=(a+7*c)/(b+2-a)+2*b
-console.log(`El resultado de (a+7*c)/(b+2-a)+2*b, es: ${formu}`)
+// 9. Escribir un programa que calcule el área y el volumen de un cilindro:
+ //A = (2 * (PI * r˄2)) + ((2 * PI * r) * h)
+ //V = (PI * r2) * h
+
+let radio = Number(prompt("Ingrese el radio del cilindro "));
+let h = Number(prompt("Ingrese la altura del cilindro "));
+let area=(2*(Math.PI * Math.pow(radio, 2)) + (2 * Math.PI * radio) * h);
+let volu=(Math.PI * Math.pow(radio, 2)) * h;
+console.log(`El area de un cilindro es: ${area}`);
+console.log(`El volumen de un cilindro es: ${volu}`);
 
 })
